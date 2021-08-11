b795ac9de8431a3c4a0032584b2b9b6a7ce0d3ac
polisen <fredrik@megatech.ltd>
2021-08-12 00:54:59 +0200

    added incredibly bad code for controls

--- src/components/ControlBar/Controls.tsx
+++ src/components/ControlBar/Controls.tsx
@@ -1,12 +1,44 @@
-import {Container} from './components'
+import { Container } from "./components";
+import styled from "styled-components";
+import { ReactComponent as PlayButton } from "../../svg/PlayButton.svg";
+import { ReactComponent as PauseButton } from "../../svg/PauseButton.svg";
+import { ReactComponent as SkipBack } from "../../svg/SkipBack.svg";
+import { ReactComponent as SkipForward } from "../../svg/SkipForward.svg";
+import { useState } from "react";
 
+const PlayPause = ({ playing }: any) => {
+  return <div>{!playing ? <PlayButton /> : <PauseButton />}</div>;
+};
 
+const Scrubber = () => {
+  return <></>;
+};
 
+const ControlDiv = styled.div`
+  width: 100%;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ControlButtons = () => {
+  const [playing, setPlaying] = useState(false);
+  return (
+    <ControlDiv onClick={() => setPlaying(!playing)}>
+      <SkipBack />
+      <PlayPause playing={playing} />
+      <SkipForward />
+    </ControlDiv>
+  );
+};
 const Controls = () => {
-    return (
-        <Container>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ControlButtons />
+      <Scrubber />
+    </Container>
+  );
+};
 
 export default Controls;
