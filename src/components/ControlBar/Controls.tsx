5904a77c06b0f5fcf51893181b01e40e5bce95de
polisen <fredrik@megatech.ltd>
2021-08-12 11:39:15 +0200

    added last remaining static components

--- src/components/ControlBar/Controls.tsx
+++ src/components/ControlBar/Controls.tsx
@@ -1,7 +1,7 @@
 import { Container } from "./components";
 import styled from "styled-components";
-import { ReactComponent as PlayButton } from "../../svg/PlayButton.svg";
-import { ReactComponent as PauseButton } from "../../svg/PauseButton.svg";
+import { ReactComponent as Play } from "../../svg/PlayButton.svg";
+import { ReactComponent as Pause } from "../../svg/PauseButton.svg";
 import { ReactComponent as SkipBack } from "../../svg/SkipBack.svg";
 import { ReactComponent as SkipForward } from "../../svg/SkipForward.svg";
 import { useState } from "react";
@@ -10,34 +10,75 @@ const PlayPause = ({ playing }: any) => {
   return <div>{!playing ? <PlayButton /> : <PauseButton />}</div>;
 };
 
+const ControlsContainer = styled(Container)`
+flex-direction: 'column';
+`
+
 const Scrubber = () => {
-  return <></>;
+  return <div
+  style={{ backgroundColor: 'white', width: '100%', height: '40%'}}
+  ></div>;
 };
 
-const ControlDiv = styled.div`
-  width: 100%;
+const ButtonContainer = styled.div`
+  width:100%;
   display: flex;
-  height: 100%;
+  height: 60%;
   justify-content: center;
-  align-items: center;
+  background-color: blue;
 `;
+
+const ButtonDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const SkipBackButton = styled(SkipBack)`
+
+`
+
+const PlayButton = styled(Play)`
+  margin: 10px;
+`
+
+const PauseButton = styled(Pause)`
+  margin: 10px;
+
+`
 
 const ControlButtons = () => {
   const [playing, setPlaying] = useState(false);
   return (
-    <ControlDiv onClick={() => setPlaying(!playing)}>
-      <SkipBack />
+    <ButtonContainer onClick={() => setPlaying(!playing)}>
+      <ButtonDiv>
+      <SkipBackButton/>
       <PlayPause playing={playing} />
       <SkipForward />
-    </ControlDiv>
+      </ButtonDiv>
+    </ButtonContainer>
   );
 };
+
+const Flex = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+height: 100%;
+align-items: center;
+
+`
 const Controls = () => {
   return (
-    <Container>
+    <ControlsContainer>
+      <Flex>
       <ControlButtons />
       <Scrubber />
-    </Container>
+      </Flex>
+    </ControlsContainer>
   );
 };
 
--- src/components/ControlBar/Volume.tsx
+++ src/components/ControlBar/Volume.tsx
@@ -1,12 +1,32 @@
-import {Container} from './components'
+import { Container } from "./components";
+import styled from "styled-components";
+import { ReactComponent as Volume } from "../../svg/Volume.svg";
+const VolumeContainer = styled(Container)`
+  justify-content: flex-end;
+`;
+const VolumeBox = styled(Container)`
+  height: 100%;
+  width: 10em;
+  background-color: red;
+`;
 
+const VolumeIcon = styled(Volume)`
+  padding: 10px;
+  height: 1em;
+  width: 1em;
+  path {
+      fill: #b3b3b3;
+    } 
+`;
 
+const VolumeLayout = () => {
+  return (
+    <VolumeContainer>
+      <VolumeBox>
+        <VolumeIcon />
+      </VolumeBox>
+    </VolumeContainer>
+  );
+};
 
-const Volume = () => {
-    return (
-        <Container>
-        </Container>
-    )
-}
-
-export default Volume;
+export default VolumeLayout;
